342340257d3029ddf3c6635280fabb0e548586e9
ashw1nm0han <ashwin.mohan.1986@gmail.com>
2013-07-28 02:24:36 -0500

    issues with bookmark notifications for events. Reverted to original. Allows git bookmark to open in a new tab.

--- javascript/parseBookmarks.js
+++ javascript/parseBookmarks.js
@@ -1,5 +1,3 @@
-var GITHUB_BOOKMARKS = 'githubbookmarks';
-var IS_LOADED = 'isNew_githubbookmarks';
 var DOM_CONTENT_LOADED = 'DOMContentLoaded';
 var ID_NO_BOOKMARKS = '#no_bookmarks';
 var MSG_NO_BOOKMARKS =  '<p><strong>No Github Bookmarks Found</strong></p>';
@@ -19,7 +17,6 @@ function createExtensionItem(bookmark){
 
 function addToPage(github_bookmarks){
 	var div = $(ID_NO_BOOKMARKS);
-	div.empty();
 	if(github_bookmarks.length > 0)
 	{
 		var list = $('<ul>');
@@ -46,16 +43,6 @@ function processBookmark(bookmark, github_bookmarks){
 	}
 }
 
-function SaveForSync(github_bookmarks){
-	try {
-		localStorage[GITHUB_BOOKMARKS] = JSON.stringify(github_bookmarks);
-		localStorage[IS_LOADED] = JSON.stringify(true);
-	}
-	catch(err){
-		console.log("The storage set for SaveForSync throws an error:" + err.message);
-	}
-}
-
 function loadMarks(){
 	chrome.bookmarks.getTree(function(bookmarks){
 	var github_bookmarks = [];
@@ -63,67 +50,15 @@ function loadMarks(){
 		processBookmark(item, github_bookmarks);
 	});
 	addToPage(github_bookmarks);
-	SaveForSync(github_bookmarks);
 	});
 
 }
 
-chrome.bookmarks.onCreated.addListener(function(id, bookmark){
-	try {
-		if(!bookmark.children && GITHUB_REGEX.test(bookmark.url)){
-			var bk = JSON.parse(localStorage[GITHUB_BOOKMARKS]);
-			bk.push({"id": bookmark.id, "title": bookmark.title, "url": bookmark.url});
-			addToPage(bk);
-			localStorage[GITHUB_BOOKMARKS] = JSON.stringify(bk);
-		}
-	}
-	catch(err){
-		console.log("The storage set for chrome.bookmarks.onCreated throws an error:" + err.message);
-	}	
-});
-
-chrome.bookmarks.onRemoved.addListener(function(){
-	loadMarks();
-});
-
-chrome.bookmarks.onImportEnded.addListener(function(){
-	loadMarks();
-});
-
-chrome.bookmarks.onChanged.addListener(function(id, changeinfo){
-	try {
-		var removeLoc = -1;
-		var bk = JSON.parse(localStorage[GITHUB_BOOKMARKS]);
-		for(var i = 0; i < bk.length; ++i){
-		
-			if(bk[i].id == id){
-				if(GITHUB_REGEX.test(bookmark.url)){
-					bk[i].title = changeinfo.title;
-					bk[i].url = changeinfo.url;
-				}
-				
-				else{
-					removeLoc = i;
-					break;
-				}
-			}
-		}
-		if(removeLoc >= 0){
-				bk.remove(removeLoc);
-		}
-		addToPage(bk);	
-		localStorage[GITHUB_BOOKMARKS] = JSON.stringify(bk);
-	}
-	catch(err){
-		console.log("The storage set for chrome.bookmarks.onCreated throws an error:" + err.message);
-	}			
-});
-
 document.addEventListener(DOM_CONTENT_LOADED, function(){
-	if(localStorage[IS_LOADED] == undefined){
-		loadMarks();
-	}
-	else {
-		addToPage(JSON.parse(localStorage[GITHUB_BOOKMARKS]));
-	}
+	loadMarks();
+});
+
+$(document).on( "click", "a", function() {
+	chrome.tabs.create({"url":this.href}, function(){});
+	return false;
 });
